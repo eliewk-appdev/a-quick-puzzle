c538584e90211d6ecee2e82ecfde4b452ed619c5
eliewk <ekhoury@chicagobooth.edu>
2020-05-12 19:40:03 +0000

    numbers updating

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,16 @@
 class PagesController < ApplicationController
   def puzzle
+    #cookies.fetch("user_id")
     render({ :template => "pages/puzzle.html.erb" })
   end
   
   def write_cookie
 
+
+   session.store(:first, params.fetch("first_num"))
+   session.store(:second, params.fetch("second_num"))
+   session.store(:third, params.fetch("third_num"))
+   
     redirect_to("/")
   end
 end
